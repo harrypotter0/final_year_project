6e7b97974e1b1c9472977e37ab04aac1cdaebe61
harrypotter0 <9654263057akashkandpal@gmail.com>
2019-04-24 08:37:34 +0530

    Tagging - Educative or Non-Educative(update)

--- src/content.js
+++ src/content.js
@@ -5,8 +5,8 @@ var wordList = read();
 addEventListener('scroll',scrollevent);
 function scrollevent(){getTitles();}
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+function getRandomInt(min, max) {
+  return Math.random() * (+max - +min) + +min; 
 }
 
 // Rating Calculation 
@@ -28,7 +28,7 @@ function getrating(title){
   console.log("Matching String "+matching_words+"\n");
   console.log("No of Matching Words is "+rate_count+"\n");
   console.log("No of Words is "+titleArray.length+"\n");  
-  
+ 
 }
 
 // Get Title of Videos
@@ -37,52 +37,54 @@ function getTitles(){
   let a_tags = document.getElementsByTagName('a');
   for(elm of a_tags){
     if (elm.id == "video-title"){
-      if (elm.id == "video-title"){
-        // var i =  getrating(elm.title.toLowerCase());
-        var title = elm.title.toLowerCase();
-  
-        var rate_count = 1;
-        var titleArray = title.split(/[ ,]+/);
-        var matching_words = ' ';
-        var i;
-        for(i in wordList){
-          if(title.includes(i.toLowerCase())){
-            // console.log(i+"\n");
-            rate_count += 1;
-            matching_words += i.toLowerCase();
-          }
+      var title = elm.title.toLowerCase();
+      var rate_count = 0;
+      // var titleArray = title.split(/[ ,]+/);
+      var titleArray = title.split(/[\s,]+/);
+      var matching_words = ' ';
+      var i;
+      for(i in wordList){
+        if(title.includes(i.toLowerCase())){
+          // console.log(i+"\n");
+          rate_count += 1;
+          matching_words += i.toLowerCase();
         }
-        // console.log("TITLE>LENGTH: "+ title.split(/(\s+)/).length);
-        tempRate = rate_count / titleArray.length * 100;
-        console.log("Rating of "+title+" is "+tempRate+"\n");
-        console.log("Matching String "+matching_words+"\n");
-        console.log("No of Matching Words is "+rate_count+"\n");
-        console.log("No of Words is "+titleArray.length+"\n");  
-        
-        if (tempRate > 20 && tempRate<50){
-          tempRate += 50;
-          if (tempRate == 100){
-            tempRate = getRandomInt(9);
-          }
-          if (tempRate >= 100){
-            tempRate = 87.1;
-          }
-        }
-        
-        if(tempRate>=50){
-          var str = new String("EDUCATIVE");
-        }
-        else{
-          var str = new String("NON EDUCATIVE");          
-        }
+      }
+      // console.log("TITLE>LENGTH: "+ title.split(/(\s+)/).length);
+      tempRate = rate_count / titleArray.length * 100;
+      console.log("Rating of "+title+" is "+tempRate+"\n");
+      console.log("Matching String "+matching_words+"\n");
+      console.log("No of Matching Words is "+rate_count+"\n");
+      console.log("No of Words is "+titleArray.length+"\n");  
 
-        console.log(elm.title.toLowerCase()+"---"+i);
-        if(str == "EDUCATIVE"){
-          elm.innerHTML = ("EV-> " + Math.round(tempRate).toString() + "% ").bold().big() + " " + str.fontcolor("green") +"    " + elm.title;
-        }
-        else{
-          elm.innerHTML = ("EV-> " + Math.round(tempRate).toString() + "% ").bold().big() + " " + str.fontcolor("red") +"    " + elm.title;
-        }
+      console.log("Title split into array+\n");
+      console.log(titleArray.toString());
+
+      if (tempRate > 0){
+        tempRate = getRandomInt(80,100);
+      }      
+      if (tempRate == 0){
+        tempRate = getRandomInt(0,10);
+      }
+      if (tempRate >= 100){
+        tempRate = 87.1;
+      }
+
+      if(tempRate>=50){
+        var str = new String("EDUCATIVE");
+      }
+      else{
+        var str = new String("NON EDUCATIVE");          
+      }
+
+      // console.log(elm.title.toLowerCase()+"---"+i);
+      if(str == "EDUCATIVE"){
+        // elm.innerHTML = ("EV-> " + Math.round(tempRate).toString() + "% ").bold().big() + " " + str.fontcolor("green") +"    " + elm.title;
+        elm.innerHTML = str.fontcolor("green").bold().big() +"    " + elm.title;
+      }
+      else{
+        // elm.innerHTML = ("EV-> " + Math.round(tempRate).toString() + "% ").bold().big() + " " + str.fontcolor("red") +"    " + elm.title;
+        elm.innerHTML = str.fontcolor("red").bold().big() +"    " + elm.title;
       }
     }
   }
@@ -90,55 +92,59 @@ function getTitles(){
   let b_tags = document.getElementsByTagName('span');
   for(elm of b_tags){
     if (elm.id == "video-title"){
-      if (elm.id == "video-title"){
-        // var i =  getrating(elm.title.toLowerCase());
-        var title = elm.title.toLowerCase();
-  
-        var rate_count = 1;
-        var titleArray = title.split(/[ ,]+/);
-        var matching_words = ' ';
-        var i;
-        for(i in wordList){
-          if(title.includes(i.toLowerCase())){
-            // console.log(i+"\n");
-            rate_count += 1;
-            matching_words += i.toLowerCase();
-          }
-        }
-        // console.log("TITLE>LENGTH: "+ title.split(/(\s+)/).length);
-        tempRate = rate_count / titleArray.length * 100;
-        console.log("Rating of "+title+" is "+tempRate+"\n");
-        console.log("Matching String "+matching_words+"\n");
-        console.log("No of Matching Words is "+rate_count+"\n");
-        console.log("No of Words is "+titleArray.length+"\n");  
-        
-        if (tempRate > 20 && tempRate<50){
-          tempRate += 50;
-          if (tempRate == 100){
-            tempRate = getRandomInt(9);
-          }
-          if (tempRate >= 100){
-            tempRate = 87.1;
-          }
-        }
-        
-        if(tempRate>=50){
-          var str = new String("EDUCATIVE");
-        }
-        else{
-          var str = new String("NON EDUCATIVE");          
-        }
-
-        console.log(elm.title.toLowerCase()+"---"+i);
-        if(str == "EDUCATIVE"){
-          elm.innerHTML = ("EV-> " + Math.round(tempRate).toString() + "% ").bold().big() + " " + str.fontcolor("green") +"    " + elm.title;
-        }
-        else{
-          elm.innerHTML = ("EV-> " + Math.round(tempRate).toString() + "% ").bold().big() + " " + str.fontcolor("red") +"    " + elm.title;
+      var title = elm.title.toLowerCase();
+      var rate_count = 0;
+      // var titleArray = title.split(/[ ,]+/);
+      var titleArray = title.split(/[\s,]+/);
+      var matching_words = ' ';
+      var i;
+      for(i in wordList){
+        if(title.includes(i.toLowerCase())){
+          // console.log(i+"\n");
+          rate_count += 1;
+          matching_words += i.toLowerCase();
         }
       }
+      // console.log("TITLE>LENGTH: "+ title.split(/(\s+)/).length);
+      tempRate = rate_count / titleArray.length * 100;
+      console.log("Rating of "+title+" is "+tempRate+"\n");
+      console.log("Matching String "+matching_words+"\n");
+      console.log("No of Matching Words is "+rate_count+"\n");
+      console.log("No of Words is "+titleArray.length+"\n");  
+
+      console.log("Title split into array+\n");
+      console.log(titleArray.toString());
+
+      if (tempRate > 0){
+        tempRate = getRandomInt(80,100);
+      }      
+      if (tempRate == 0){
+        tempRate = getRandomInt(0,10);
+      }
+      if (tempRate >= 100){
+        tempRate = 87.1;
+      }
+
+      if(tempRate>=50){
+        var str = new String("EDUCATIVE");
+      }
+      else{
+        var str = new String("NON EDUCATIVE");          
+      }
+
+      // console.log(elm.title.toLowerCase()+"---"+i);
+      if(str == "EDUCATIVE"){
+        // elm.innerHTML = ("EV-> " + Math.round(tempRate).toString() + "% ").bold().big() + " " + str.fontcolor("green") +"    " + elm.title;
+        elm.innerHTML = str.fontcolor("green").bold().big() +"    " + elm.title;
+      }
+      else{
+        // elm.innerHTML = ("EV-> " + Math.round(tempRate).toString() + "% ").bold().big() + " " + str.fontcolor("red") +"    " + elm.title;
+        elm.innerHTML = str.fontcolor("red").bold().big() +"    " + elm.title;
+      }
     }
+
   }
+
 }
 
 function read(){
@@ -148,7 +154,7 @@ function read(){
     if(xmlhttp.status == 200 && xmlhttp.readyState == 4){}
   };
   // Send request to server
-  var link = "https://gist.githubusercontent.com/harrypotter0/d235733379d4994fca01a52e36fc5be9/raw/e1da226b2b8f13b4228efcfd735a319396b497f8/wordlist.txt";
+  var link = "https://gist.githubusercontent.com/harrypotter0/d235733379d4994fca01a52e36fc5be9/raw/b0092319c16460bd9b434b4d4a7c80d1eff313f5/wordlist.txt";
   xmlhttp.open("GET", link, true);
   xmlhttp.send();
   return this;
